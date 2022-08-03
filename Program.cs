7cb8c29900e878cd416322adcb01ded645008dfb
Faysal Regal <faysalregal@gmail.com>
2022-08-03 20:53:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var dateTime = new DateTime(2015, 1, 1);
+var now = DateTime.Now;
+var today = DateTime.Today;
+
+Console.WriteLine("Hour: " + now.Hour);
+Console.WriteLine("Min1: " + now.Minute);
